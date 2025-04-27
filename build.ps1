d878f856a61454be48c2fd526c54c75130682f9c
ugai <ugai.sub1@gmail.com>
2025-04-27 21:29:08 +0900

    Add try-finally blocks for better error handling in build process

--- build.ps1
+++ build.ps1
@@ -38,10 +38,15 @@ if (-Not $SkipBoost) {
     }
 
     Push-Location boost
+
+    try {
     .\bootstrap.bat
     Write-Output "using python : : $PythonRoot ;" > user-config.jam
     .\b2 --build-type=complete --variant=release --with-python --user-config=user-config.jam
+    }
+    finally {
     Pop-Location
+    }
 }
 
 # Build imath
@@ -49,6 +54,7 @@ if (-Not $SkipImath) {
     git clone https://github.com/AcademySoftwareFoundation/Imath
 
     Push-Location Imath
+    try {
     # The commit below from the Imath repo causes a PyAlembic build error:
     #
     #   src/python/config: do not install a cmake file exporting targets for dependent projects #361
@@ -56,16 +62,24 @@ if (-Not $SkipImath) {
     #
     # Checkout the previous commit to avoid the error.
     git checkout 84f9a674802f6c3197bd478c9b40399f451fecb3
+    }
+    finally {
     Pop-Location
+    }
 
     & $PythonExe -m pip install numpy
 
     if (!(Test-Path Imath/build)) { mkdir Imath/build }
     Push-Location Imath/build
+
+    try {
     cmake .. -DPython_EXECUTABLE="$PythonExe" -DPython3_EXECUTABLE="$PythonExe" -DPYTHON=ON -DBoost_ROOT="../../$BoostZipDestName" -DCMAKE_INSTALL_PREFIX="../_installed"
     cmake --build . --config Release
     cmake --install .
+    }
+    finally {
     Pop-Location
+    }
 }
 
 # Build alembic
@@ -73,10 +87,15 @@ if (-Not $SkipAlembic) {
     git clone https://github.com/alembic/alembic
     if (!(Test-Path alembic/build)) { mkdir alembic/build }
     Push-Location alembic/build
+
+    try {
     cmake .. -DUSE_PYALEMBIC=ON -DImath_DIR="../Imath/_installed/lib/cmake/Imath" -DPython3_EXECUTABLE="$PythonExe" -DBoost_ROOT="../../$BoostZipDestName" -DCMAKE_INSTALL_PREFIX="../_installed" -DALEMBIC_PYTHON_INSTALL_DIR="../_installed/lib/site-packages"
     cmake --build . --config Release
     cmake --install .
+    }
+    finally {
     Pop-Location
+    }
 }
 
 # Create wheel package
